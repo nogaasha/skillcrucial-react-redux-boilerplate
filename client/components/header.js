b6ce5672316aa5ebd1bd7ce894fe26ace6a3f4d3
Nogaasha <sivosha2010@gmail.com>
2020-07-16 17:13:09 +0300

    change routes2

--- client/components/header.js
+++ client/components/header.js
@@ -32,7 +32,7 @@ const Header = () => {
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             type="button"
           >
-            <Link to="/dashboard/">First Page</Link>
+            <Link to="/">First Page</Link>
           </button>
         </div>
       </div>
--- client/components/home.js
+++ client/components/home.js
@@ -15,15 +15,12 @@ const Home = () => {
         <div className="flex items-start justify-center h-screen">
           <div className="bg-indigo-800  text-white font-bold rounded-lg border shadow-lg p-10">
             <Switch>
-              <Route exact path="/dashboard/" component={() => <Dashboard />} />
+              <Route exact path="/" component={() => <Dashboard />} />
               <Route exact path="/:username" component={() => <Repositories />} />
               <Route exact path="/:username/:reponame" component={() => <Profile />} />
             </Switch>
             <div>
-              <Link to="/dashboard/"> Go To First Page </Link>
-            </div>
-            <div>
-              <Link to="/"> Go To Home </Link>
+              <Link to="/"> Go To First Page</Link>
             </div>
           </div>
         </div>
